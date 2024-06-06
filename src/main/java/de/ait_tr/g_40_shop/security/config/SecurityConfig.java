6ca5ef83f2a3ff78ed7cdae9fdf271f5ae63a8a7
danbaliaba <danbaliab@gmail.com>
2024-06-06 15:49:11 +0200

    Cart_void_deleteProductById

---
+++ src/main/java/de/ait_tr/g_40_shop/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package de.ait_tr.g_40_shop.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return null;
+    }
+}
---
+++ src/main/java/de/ait_tr/g_40_shop/service/UserService.java
@@ -0,0 +1,31 @@
+package de.ait_tr.g_40_shop.service;
+
+import de.ait_tr.g_40_shop.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+    private UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(
+                        String.format("User %s not found", username)
+                )
+        );
+        // Альтернативный вариант
+//        User user = repository.findByUsername(username);
+//
+//        if (user == null) {
+//            throw new UsernameNotFoundException(
+//                    String.format("User %s not found", username));
+//        }
+//
+//        return user;
+    }
+}
