88ef7622315cd2b5a6888d23d4776a79dacb92dc
danbaliaba <danbaliab@gmail.com>
2024-06-14 15:23:28 +0200

    Lesson_15_HW AlreadyExistingProductException DeletedProductException ProductNotFoundException

--- src/main/java/de/ait_tr/g_40_shop/exception_handling/exceptions/ThirdTestException.java
+++
@@ -1,9 +0,0 @@
-package de.ait_tr.g_40_shop.exception_handling.exceptions;
-
-public class ThirdTestException extends RuntimeException{
-
-    public ThirdTestException(String message) {
-        super(message);
-    }
-
-}
